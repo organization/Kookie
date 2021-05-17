dc6c0bb827553285cbec4a6cc9abc3465604da59
JellyBrick <shlee1503@naver.com>
2021-05-18 01:46:48 +0900

    Update LICENSE header

--- app/src/main/kotlin/be/zvz/kookie/utils/Union.kt
+++ app/src/main/kotlin/be/zvz/kookie/utils/Union.kt
@@ -1,19 +1,17 @@
 /**
+ * Copyright 2017 Renato Athaydes. All rights reserved.
  *
- * _  __           _    _
- * | |/ /___   ___ | | _(_) ___
- * | ' // _ \ / _ \| |/ / |/ _ \
- * | . \ (_) | (_) |   <| |  __/
- * |_|\_\___/ \___/|_|\_\_|\___|
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * A server software for Minecraft: Bedrock Edition
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright (C) 2021 organization Team
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package be.zvz.kookie.utils
 
