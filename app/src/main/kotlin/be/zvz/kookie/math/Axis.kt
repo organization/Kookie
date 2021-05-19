3eae4c1661812832387fc4822f1c4d1b11f3a5e9
alvin0319 <alvin7810@naver.com>
2021-05-20 00:44:59 +0900

    Fixed syntax error

--- app/src/main/kotlin/be/zvz/kookie/math/Axis.kt
+++ app/src/main/kotlin/be/zvz/kookie/math/Axis.kt
@@ -1,7 +1,7 @@
 package be.zvz.kookie.math
 
 enum class Axis(val value: Int) {
-    X(0)
-    Y(1)
+    X(0),
+    Y(1),
     Z(2)
 }
--- app/src/main/kotlin/be/zvz/kookie/math/Facing.kt
+++ app/src/main/kotlin/be/zvz/kookie/math/Facing.kt
@@ -5,12 +5,12 @@ class Facing {
     companion object {
         private const val FLAG_AXIS_POSITIVE = 1
 
-        val DOWN = Axis.Y shl 1
-        val UP = (Axis.Y shl 1) or FLAG_AXIS_POSITIVE
-        val NORTH = Axis.Z shl 1
-        val SOUTH = (Axis.Z shl 1) or FLAG_AXIS_POSITIVE
-        val WEST = Axis.X shl 1
-        val EAST = (Axis.X shl 1) or FLAG_AXIS_POSITIVE
+        val DOWN = Axis.Y.value shl 1
+        val UP = (Axis.Y.value shl 1) or FLAG_AXIS_POSITIVE
+        val NORTH = Axis.Z.value shl 1
+        val SOUTH = (Axis.Z.value shl 1) or FLAG_AXIS_POSITIVE
+        val WEST = Axis.X.value shl 1
+        val EAST = (Axis.X.value shl 1) or FLAG_AXIS_POSITIVE
 
         val ALL = arrayOf(DOWN, UP, NORTH, SOUTH, WEST, EAST)
 
