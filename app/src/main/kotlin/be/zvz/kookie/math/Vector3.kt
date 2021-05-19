385661562361742d756086e78805b4fc59dcb371
alvin0319 <alvin7810@naver.com>
2021-05-20 01:01:34 +0900

    Added missing header

--- app/src/main/kotlin/be/zvz/kookie/math/Vector3.kt
+++ app/src/main/kotlin/be/zvz/kookie/math/Vector3.kt
@@ -1,3 +1,20 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 package be.zvz.kookie.math
 
 class Vector3 @JvmOverloads constructor(var x: Float = 0F, var y: Float = 0F, var z: Float = 0F) {
