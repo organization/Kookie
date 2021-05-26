7016ffce7b6e5b2b61a4b697d28a9a4f3a4cf7ec
LeinneSW <nno88551@naver.com>
2021-05-26 15:08:59 +0900

    Move ItemTypeEntry

--- app/src/main/kotlin/be/zvz/kookie/network/mcpe/protocol/types/ItemTypeEntry.kt
+++ app/src/main/kotlin/be/zvz/kookie/network/mcpe/protocol/types/ItemTypeEntry.kt
@@ -1,10 +1,3 @@
 package be.zvz.kookie.network.mcpe.protocol.types
 
-class ItemTypeEntry(private val stringId: String, private val numericId: Int, private val componentBased: Boolean) {
-
-    fun getStringId(): String = stringId
-
-    fun getNumericId(): Int = numericId
-
-    fun isComponentBased(): Boolean = componentBased
-}
+data class ItemTypeEntry(val stringId: String, val numericId: Int, var componentBased: Boolean)
