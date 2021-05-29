672e57d5ce54aaa8226ba2bf0346e1bb7ebdcff5
LeinneSW <nno88551@naver.com>
2021-05-30 05:35:58 +0900

    CacheableNbt: encodedNbt property always return String - make non-null - remove setter (var -> val) - use lazy init(by lazy)

--- app/src/main/kotlin/be/zvz/kookie/network/mcpe/protocol/types/CacheableNbt.kt
+++ app/src/main/kotlin/be/zvz/kookie/network/mcpe/protocol/types/CacheableNbt.kt
@@ -6,12 +6,5 @@ import be.zvz.kookie.network.mcpe.protocol.NetworkNbtSerializer
 
 class CacheableNbt(val root: Tag<*>) {
 
-    var encodedNbt: String? = null
-        get() {
-            if (field === null) {
-                field = NetworkNbtSerializer().write((TreeRoot(root)))
-            }
-            return field
-        }
-        private set
+    val encodedNbt: String by lazy { NetworkNbtSerializer().write((TreeRoot(root))) }
 }
