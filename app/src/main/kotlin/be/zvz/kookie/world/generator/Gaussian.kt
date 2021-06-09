b8b994e36ad836e359886860af1e46b67048ce7c
PresentKim <debe3721@gmail.com>
2021-06-09 18:17:11 +0900

    world : Implements Generator and Gaussian

---
+++ app/src/main/kotlin/be/zvz/kookie/world/generator/Gaussian.kt
@@ -0,0 +1,41 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.world.generator
+
+import com.koloboke.collect.map.hash.HashIntObjMaps
+import kotlin.math.exp
+
+class Gaussian(val smoothSize: Int) {
+    val kernel: Map<Int, Map<Int, Float>>
+
+    init {
+        val bellSize = 1F / smoothSize
+        val bellHeight = 2F * smoothSize
+
+        kernel = HashIntObjMaps.newMutableMap()
+        for (sx in (-smoothSize)..smoothSize) {
+            val szMap = HashIntObjMaps.newMutableMap<Float>()
+            for (sz in (-smoothSize)..smoothSize) {
+                val bx = bellSize * sx
+                val bz = bellSize * sz
+                szMap[sz + smoothSize] = bellHeight * exp((bx * bx + bz * bz) / 2)
+            }
+            kernel[sx + smoothSize] = szMap
+        }
+    }
+}
---
+++ app/src/main/kotlin/be/zvz/kookie/world/generator/Generator.kt
@@ -0,0 +1,59 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.world.generator
+
+import be.zvz.kookie.world.ChunkManager
+import java.util.Random
+
+abstract class Generator(
+    protected val seed: Long,
+    protected val preset: String
+) {
+    protected val random = Random(seed)
+
+    abstract fun generateChunk(world: ChunkManager, chunkX: Int, chunkZ: Int)
+
+    abstract fun populateChunk(world: ChunkManager, chunkX: Int, chunkZ: Int)
+
+    companion object {
+        fun convertSeed(seed: String): Long? = when {
+            seed == "" -> null
+            seed.matches(Regex("^-?\\d+\$")) -> seed.toLong()
+            else -> javaStringHash(seed)
+        }
+
+        private fun javaStringHash(seed: String): Long {
+            var hash = 0L
+            for (i in seed.indices) {
+                var ord = seed[i].code
+                if (ord and 0x80 != 0) {
+                    ord -= 0x100
+                }
+                hash = hash * 31 + ord
+                while (hash > 0x7FFFFFFF) {
+                    hash -= 0x100000000
+                }
+                while (hash < -0x80000000) {
+                    hash += 0x100000000
+                }
+                hash = hash and 0xFFFFFFFF
+            }
+            return hash
+        }
+    }
+}
