940e62a19a4c4d36280d6b4328c210e0a01f8587
PresentKim <debe3721@gmail.com>
2021-06-09 20:15:06 +0900

    Chunk : Implements missing method and fields

--- app/src/main/kotlin/be/zvz/kookie/world/format/Chunk.kt
+++ app/src/main/kotlin/be/zvz/kookie/world/format/Chunk.kt
@@ -20,7 +20,10 @@ package be.zvz.kookie.world.format
 import be.zvz.kookie.block.BlockLegacyIds
 import be.zvz.kookie.block.tile.Tile
 import be.zvz.kookie.entity.Entity
+import be.zvz.kookie.math.Vector3
+import be.zvz.kookie.player.Player
 import be.zvz.kookie.world.biome.BiomeIds
+import com.koloboke.collect.map.hash.HashIntObjMaps
 
 class Chunk(
     val subChunks: MutableList<SubChunk> = MutableList(MAX_SUBCHUNKS) {
@@ -31,25 +34,116 @@ class Chunk(
     val biomeIds: BiomeArray = BiomeArray.fill(BiomeIds.OCEAN.id),
     val heightMap: HeightArray = HeightArray.fill(subChunks.size * 16)
 ) {
-    private var dirtyFlags: Int = 0
+    var dirtyFlags: Int = 0
+    val tiles = HashIntObjMaps.newMutableMap<Tile>()
+    val entities = HashIntObjMaps.newMutableMap<Entity>()
 
-    fun getHeight(): Int = subChunks.size
+    var lightPopulated = false
+    var terrainPopulated = false
+
+    val height: Int = subChunks.size
+
+    val savableEntities = entities.filter {
+        TODO("Implements after implemented Entity::canSaveWithChunk()")
+    }
 
     fun getFullBlock(x: Int, y: Int, z: Int): Long {
         return getSubChunk(y shr 4).getFullBlock(x, y and 0xf, z)
     }
 
-    fun getSubChunk(y: Int): SubChunk {
-        if (y !in 0 until subChunks.size) {
-            throw IllegalArgumentException("Invalid subchunk Y coordinate $y")
-        }
-
-        return subChunks[y]
-    }
-
     fun setFullBlock(x: Int, y: Int, z: Int, block: Long) {
         getSubChunk(y shr 4).setFullBlock(x, y and 0xf, z, block)
         dirtyFlags = dirtyFlags or DIRTY_FLAG_TERRAIN
+    }
+
+    fun getHighestBlockAt(x: Int, z: Int): Int? {
+        for (y in (subChunks.size - 1)..0 step (-1)) {
+            val height = getSubChunk(y).getHighestBlockAt(x, z)
+            if (height != null) {
+                return height or (y shl 4)
+            }
+        }
+
+        return null
+    }
+
+    fun getHeightMap(x: Int, z: Int): Int = heightMap[x, z]
+    fun setHeightMap(x: Int, z: Int, value: Int) {
+        heightMap[x, z] = value
+    }
+
+    fun getBiomeId(x: Int, z: Int): Int = biomeIds[x, z]
+    fun setBiomeId(x: Int, z: Int, value: Int) {
+        biomeIds[x, z] = value
+    }
+
+    fun addEntity(entity: Entity) {
+        TODO("Implements after implemented Entity::isClosed()")
+    }
+
+    fun removeEntity(entity: Entity) {
+        TODO("Implements after implemented Entity::isClosed()")
+    }
+
+    fun addTile(tile: Tile) {
+        if (tile.closed) {
+            throw IllegalArgumentException("Attempted to add a garbage closed Tile to a chunk")
+        }
+
+        val index = blockHash(tile.pos)
+        if (tiles[index] != null && tiles[index] !== tile) {
+            throw IllegalArgumentException("Another tile is already at this location")
+        }
+
+        tiles[index] = tile
+        setDirtyFlag(DIRTY_FLAG_TILES, true)
+    }
+
+    fun removeTile(tile: Tile) {
+        val index = blockHash(tile.pos)
+        if (tiles[index] != null) {
+            tiles.remove(index)
+            setDirtyFlag(DIRTY_FLAG_TILES, true)
+        }
+    }
+
+    fun getTile(pos: Vector3) = getTile(pos.x.toInt(), pos.y.toInt(), pos.z.toInt())
+    fun getTile(x: Int, y: Int, z: Int) = tiles[blockHash(x, y, z)]
+
+    fun onUnload() {
+        entities.forEach { (_, entity) ->
+            if (entity !is Player) {
+                TODO("Implements after implemented Entity::close()")
+            }
+        }
+        tiles.forEach { (_, tile) ->
+            tile.close()
+        }
+    }
+
+    fun getDirtyFlag(flag: Int) = (dirtyFlags and flag) != 0
+    fun setDirtyFlag(flag: Int, value: Boolean) {
+        dirtyFlags = if (value) {
+            dirtyFlags or flag
+        } else {
+            dirtyFlags and flag.inv()
+        }
+    }
+
+    fun getSubChunk(y: Int) =
+        if (y !in 0 until subChunks.size) {
+            throw IllegalArgumentException("Invalid subchunk Y coordinate $y")
+        } else {
+            subChunks[y]
+        }
+
+    fun setSubChunk(y: Int, subChunk: SubChunk?) {
+        if (y !in 0 until subChunks.size) {
+            throw IllegalArgumentException("Invalid subchunk Y coordinate $y")
+        } else {
+            subChunks[y] = subChunk ?: SubChunk(BlockLegacyIds.AIR.id.toLong() shl 4, mutableListOf())
+            setDirtyFlag(DIRTY_FLAG_TERRAIN, true)
+        }
     }
 
     companion object {
@@ -59,5 +153,8 @@ class Chunk(
         const val DIRTY_FLAG_BIOMES = 1 shl 3
 
         const val MAX_SUBCHUNKS = 16
+
+        fun blockHash(pos: Vector3) = blockHash(pos.x.toInt(), pos.y.toInt(), pos.z.toInt())
+        fun blockHash(x: Int, y: Int, z: Int) = (y shl 8) or ((z and 0x0f) shl 4) or (x and 0x0f)
     }
 }
