c57ecc8cff87d38485b208d7875fd6499a4abe40
PresentKim <debe3721@gmail.com>
2021-06-12 23:12:24 +0900

    types : Add ParticleIds for add const data

---
+++ app/src/main/kotlin/be/zvz/kookie/network/mcpe/protocol/types/ParticleIds.kt
@@ -0,0 +1,100 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.network.mcpe.protocol.types
+
+object ParticleIds {
+    const val BUBBLE = 1
+    const val BUBBLE_MANUAL = 2
+    const val CRITICAL = 3
+    const val BLOCK_FORCE_FIELD = 4
+    const val SMOKE = 5
+    const val EXPLODE = 6
+    const val EVAPORATION = 7
+    const val FLAME = 8
+    const val LAVA = 9
+    const val LARGE_SMOKE = 10
+    const val REDSTONE = 11
+    const val RISING_RED_DUST = 12
+    const val ITEM_BREAK = 13
+    const val SNOWBALL_POOF = 14
+    const val HUGE_EXPLODE = 15
+    const val HUGE_EXPLODE_SEED = 16
+    const val MOB_FLAME = 17
+    const val HEART = 18
+    const val TERRAIN = 19
+    const val SUSPENDED_TOWN = 20
+    const val PORTAL = 21
+    const val WATER_SPLASH = 23
+    const val WATER_SPLASH_MANUAL = 24
+    const val WATER_WAKE = 25
+    const val DRIP_WATER = 26
+    const val DRIP_LAVA = 27
+    const val DRIP_HONEY = 28
+    const val STALACTITE_DRIP_WATER = 29
+    const val STALACTITE_DRIP_LAVA = 30
+    const val DUST = 31
+    const val MOB_SPELL = 32
+    const val MOB_SPELL_AMBIENT = 33
+    const val MOB_SPELL_INSTANTANEOUS = 34
+    const val INK = 35
+    const val SLIME = 36
+    const val RAIN_SPLASH = 37
+    const val VILLAGER_ANGRY = 38
+    const val VILLAGER_HAPPY = 39
+    const val ENCHANTMENT_TABLE = 40
+    const val TRACKING_EMITTER = 41
+    const val NOTE = 42
+    const val WITCH_SPELL = 43
+    const val CARROT = 44
+    const val MOB_APPEARANCE = 45
+    const val END_ROD = 46
+    const val DRAGONS_BREATH = 47
+    const val SPIT = 48
+    const val TOTEM = 49
+    const val FOOD = 50
+    const val FIREWORKS_STARTER = 51
+    const val FIREWORKS_SPARK = 52
+    const val FIREWORKS_OVERLAY = 53
+    const val BALLOON_GAS = 54
+    const val COLORED_FLAME = 55
+    const val SPARKLER = 56
+    const val CONDUIT = 57
+    const val BUBBLE_COLUMN_UP = 58
+    const val BUBBLE_COLUMN_DOWN = 59
+    const val SNEEZE = 60
+    const val SHULKER_BULLET = 61
+    const val BLEACH = 62
+    const val DRAGON_DESTROY_BLOCK = 63
+    const val MYCELIUM_DUST = 64
+    const val FALLING_RED_DUST = 65
+    const val CAMPFIRE_SMOKE = 66
+    const val TALL_CAMPFIRE_SMOKE = 67
+    const val DRAGON_BREATH_FIRE = 68
+    const val DRAGON_BREATH_TRAIL = 69
+    const val BLUE_FLAME = 70
+    const val SOUL = 71
+    const val OBSIDIAN_TEAR = 72
+    const val PORTAL_REVERSE = 73
+    const val SNOWFLAKE = 74
+    const val VIBRATION_SIGNAL = 75
+    const val SCULK_SENSOR_REDSTONE = 76
+    const val SPORE_BLOSSOM_SHOWER = 77
+    const val SPORE_BLOSSOM_AMBIENT = 78
+    const val WAX = 79
+    const val ELECTRIC_SPARK = 80
+}
