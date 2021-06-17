a99ada016e884419c698dc5b230849d72845554d
PresentKim <debe3721@gmail.com>
2021-06-18 00:12:35 +0900

    world : Add interfaces for world

---
+++ app/src/main/kotlin/be/zvz/kookie/world/ChunkLoader.kt
@@ -0,0 +1,29 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.world
+
+/**
+ * If you want to keep chunks loaded, implement this interface and register it into World.
+ *
+ * @see World::registerChunkLoader()
+ * @see World::unregisterChunkLoader()
+ *
+ * WARNING: When moving this object around in the world or destroying it,
+ * be sure to unregister the loader from chunks you're not using, otherwise you'll leak memory.
+ */
+interface ChunkLoader
---
+++ app/src/main/kotlin/be/zvz/kookie/world/TickingChunkLoader.kt
@@ -0,0 +1,28 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.world
+
+/**
+ * TickingChunkLoader includes all of the same functionality as ChunkLoader (it can be used in the same way).
+ * However, using this version will also cause chunks around the loader's reported coordinates to get random block
+ * updates.
+ */
+interface TickingChunkLoader : ChunkLoader {
+    fun getX(): Float
+    fun getZ(): Float
+}
