7353b52df83ded8d7d68422153d6c47e042e84e2
alvin0319 <alvin7810@naver.com>
2021-06-18 01:19:35 +0900

    Removed duplicate class

--- app/src/main/kotlin/be/zvz/kookie/entity/effect/PoisonEffect.kt
+++ app/src/main/kotlin/be/zvz/kookie/entity/effect/PoisonEffect.kt
@@ -31,25 +31,21 @@ class PoisonEffect(
 ) : Effect(internalRuntimeId, name, color, bad, hasBubbles) {
 
     override fun canTick(instance: EffectInstance): Boolean {
-        /*
-        if(($interval = (25 >> $instance->getAmplifier())) > 0){
-			return ($instance->getDuration() % $interval) === 0;
-		}
-         */
         val interval = 25 shr instance.amplifier
-        if (interval > 0) {
-            return instance.duration % interval == 0
+        return if (interval > 0) {
+            instance.duration % interval == 0
+        } else {
+            true
         }
-        return true
     }
 
     override fun applyEffect(entity: Living, instance: EffectInstance, potency: Float, source: Entity?) {
-        /*
-        TODO:
-        if($entity->getHealth() > 1 or $this->fatal){
-			$ev = new EntityDamageEvent($entity, EntityDamageEvent::CAUSE_MAGIC, 1);
-			$entity->attack($ev);
-		}
-         */
+        if (entity.getHealth() > 1 || fatal) {
+            /*
+            TODO:
+            val ev = EntityDamageEvent(entity, EntityDamageEvent.CAUSE_MAGIC, 1)
+            entity.attack(ev)
+             */
+        }
     }
 }
