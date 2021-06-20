05ffe197ff67dc797655d0a6657a697f19c545ac
alvin0319 <alvin7810@naver.com>
2021-06-21 08:40:32 +0900

    feature: Crafting (#36)

---
+++ app/src/main/kotlin/be/zvz/kookie/crafting/CraftingManagerFromDataHelper.kt
@@ -0,0 +1,99 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.crafting
+
+import be.zvz.kookie.crafting.utils.FurnaceRecipeData
+import be.zvz.kookie.crafting.utils.ShapedRecipeData
+import be.zvz.kookie.crafting.utils.ShapelessRecipeData
+import be.zvz.kookie.item.Item
+import be.zvz.kookie.item.ItemFactory
+import be.zvz.kookie.utils.Json
+import com.fasterxml.jackson.module.kotlin.convertValue
+import java.io.BufferedInputStream
+import java.io.File
+
+object CraftingManagerFromDataHelper {
+    @JvmStatic
+    fun make(file: File): CraftingManager {
+        if (!file.exists()) {
+            throw IllegalStateException("Expected file at ${file.path}, but none found")
+        }
+        val result = CraftingManager()
+
+        val recipes = file.inputStream().use {
+            BufferedInputStream(it).use(Json.jsonMapper::readTree)
+        }
+        recipes["shapeless"].let {
+            Json.jsonMapper.convertValue<List<ShapelessRecipeData>>(it).forEach { recipe ->
+                if (recipe.block != "crafting_table") {
+                    return@forEach
+                }
+                val inputs = mutableListOf<Item>().apply {
+                    recipe.input.forEach { item ->
+                        add(ItemFactory.get(item.id, item.damage, 1))
+                    }
+                }
+                val outputs = mutableListOf<Item>().apply {
+                    recipe.output.forEach { item ->
+                        add(ItemFactory.get(item.id, item.damage, 1))
+                    }
+                }
+                result.registerShapelessRecipe(
+                    ShapelessRecipe(
+                        inputs,
+                        outputs
+                    )
+                )
+            }
+        }
+        recipes["shaped"].let {
+            Json.jsonMapper.convertValue<List<ShapedRecipeData>>(it).forEach { recipe ->
+                if (recipe.block != "crafting_table") {
+                    return@forEach
+                }
+                val input = mutableMapOf<String, Item>().apply {
+                    recipe.input.forEach { (regex, data) ->
+                        put(regex, ItemFactory.get(data.id, data.damage, 1))
+                    }
+                }
+                val output = mutableListOf<Item>().apply {
+                    recipe.output.forEach { data ->
+                        add(ItemFactory.get(data.id, data.damage, 1))
+                    }
+                }
+                result.registerShapedRecipe(
+                    ShapedRecipe(recipe.shape, input, output)
+                )
+            }
+        }
+        recipes["smelting"].let {
+            Json.jsonMapper.convertValue<List<FurnaceRecipeData>>(it).forEach { recipe ->
+                if (recipe.block != "furnace") {
+                    return@forEach
+                }
+                result.furnaceRecipeManager.register(
+                    FurnaceRecipe(
+                        ItemFactory.get(recipe.input.id, recipe.input.damage, 1),
+                        ItemFactory.get(recipe.output.id, recipe.output.damage, 1)
+                    )
+                )
+            }
+        }
+        return result
+    }
+}
