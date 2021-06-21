387f869be95289996159f697349a1a8fc4de8008
Kintiru <Kintiru@gmail.com>
2021-06-22 00:38:51 +0900

    Localized HorizontalFacingHelper.facing in Anvil.kt

--- app/src/main/kotlin/be/zvz/kookie/block/Anvil.kt
+++ app/src/main/kotlin/be/zvz/kookie/block/Anvil.kt
@@ -1,25 +1,54 @@
 package be.zvz.kookie.block
 
 //import be.zvz.kookie.block.inventory.AnvilInventory
-//import be.zvz.kookie.block.util.BlockDataSeroializer
+//import be.zvz.kookie.block.util.BlockDataSerializer
 import be.zvz.kookie.block.util.Fallable
 import be.zvz.kookie.block.util.FallableHelper
+import be.zvz.kookie.block.util.HorizontalFacingHelper
+import be.zvz.kookie.math.Axis
+import be.zvz.kookie.math.AxisAlignedBB
 import be.zvz.kookie.math.Facing
 
 class Anvil(
     idInfo: BlockIdentifier,
     name: String,
     breakInfo: BlockBreakInfo
-) : Transparent(idInfo, name, breakInfo), Fallable, FallableHelper {
+) : Transparent(idInfo, name, breakInfo), Fallable, FallableHelper, HorizontalFacingHelper {
     private var damage: Int = 0
+    var facing: Facing = HorizontalFacingHelper.facing
 
     override fun writeStateToMeta(): Int {
         //TODO: not yet implemented
     }
 
     override fun readStateFromData(id: Int, stateMeta: Int) {
-        Facing
+        //TODO: remove comment after implementing functions
+        //facing = BlockDataSerializer.readLegacyHorizontalFacing(stateMeta and 0x3)
+        //damage = BlockDataSerializer.readBoundedInt("damage", stateMeta shr 2, 0, 2)
     }
+
+    override fun getStateBitmask(): Int = 0b1111
+
+    override fun getNonPersistentStateBitmask(): Int = 0b11
+
+    fun getDamage(): Int = damage
+
+    fun setDamage(damage: Int): Anvil = apply {
+        if(damage < 0 || damage > 2) {
+            throw IllegalArgumentException("Damage must be in range 0-2")
+        }
+        this.damage = damage
+    }
+
+    override fun recalculateCollisionBoxes(): List<AxisAlignedBB> = listOf(
+        AxisAlignedBB.one().squash( //TODO: Conflict between squash and Facing.axis
+            Facing.axis(
+                Facing.rotate(
+                    facing, Axis.Y ,false
+                )
+            ),
+            1/8)
+    )
 
     override fun tickFalling(): Block? = null
 }
