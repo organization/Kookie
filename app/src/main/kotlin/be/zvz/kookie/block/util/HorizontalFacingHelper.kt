1ca72ee639c1dca907181ca698e8d4a51b752168
Kintiru <Kintiru@gmail.com>
2021-06-21 23:09:29 +0900

    create HorizontalFacingHelper.kt

--- app/src/main/kotlin/be/zvz/kookie/block/util/HorizontalFacingHelper.kt
+++ app/src/main/kotlin/be/zvz/kookie/block/util/HorizontalFacingHelper.kt
@@ -1,8 +1,17 @@
 package be.zvz.kookie.block.util
 
+import be.zvz.kookie.math.Axis
 import be.zvz.kookie.math.Facing
 
 interface HorizontalFacingHelper {
-    val facing: Facing
-        get() = Facing.NORTH
+    var facing: Int
+        get() = Facing.NORTH.value
+        set(value) {
+            val axis = Facing.axis(value)
+            if(axis != Axis.X.value && axis != Axis.Z.value) {
+                throw IllegalArgumentException("Facing must be horizontal")
+            }
+            //TODO: need review (return & recursive access)
+            facing = value
+        }
 }
