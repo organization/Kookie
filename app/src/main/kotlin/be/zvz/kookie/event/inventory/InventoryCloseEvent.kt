ec989461ebecccf64d7a935a05c7d80f101a028d
codefactor-io <support@codefactor.io>
2021-06-21 01:47:38 +0000

    [CodeFactor] Apply fixes to commit 7719530

--- app/src/main/kotlin/be/zvz/kookie/event/inventory/InventoryCloseEvent.kt
+++ app/src/main/kotlin/be/zvz/kookie/event/inventory/InventoryCloseEvent.kt
@@ -3,5 +3,4 @@ package be.zvz.kookie.event.inventory
 import be.zvz.kookie.inventory.Inventory
 import be.zvz.kookie.player.Player
 
-class InventoryCloseEvent(inventory: Inventory, val who: Player) : InventoryEvent(inventory) {
-}
+class InventoryCloseEvent(inventory: Inventory, val who: Player) : InventoryEvent(inventory)
