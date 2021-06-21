a4288b2622c3be86da7785e4efaf2bfb8c1c231b
Kintiru <Kintiru@gmail.com>
2021-06-22 00:38:07 +0900

    create companion object in HorizontalFacingHelper.kt

--- app/src/main/kotlin/be/zvz/kookie/block/util/HorizontalFacingHelper.kt
+++ app/src/main/kotlin/be/zvz/kookie/block/util/HorizontalFacingHelper.kt
@@ -4,14 +4,16 @@ import be.zvz.kookie.math.Axis
 import be.zvz.kookie.math.Facing
 
 interface HorizontalFacingHelper {
-    var facing: Int
-        get() = Facing.NORTH.value
-        set(value) {
-            val axis = Facing.axis(value)
-            if(axis != Axis.X.value && axis != Axis.Z.value) {
-                throw IllegalArgumentException("Facing must be horizontal")
+    companion object {
+        var facing: Facing = Facing.NORTH
+            get() = Facing.NORTH
+            set(value) {
+                val axis = Facing.axis(value.value)
+                if (axis != Axis.X.value && axis != Axis.Z.value) {
+                    throw IllegalArgumentException("Facing must be horizontal")
+                }
+                //TODO: need review (if return value is needed)
+                field = value
             }
-            //TODO: need review (return & recursive access)
-            facing = value
-        }
+    }
 }
