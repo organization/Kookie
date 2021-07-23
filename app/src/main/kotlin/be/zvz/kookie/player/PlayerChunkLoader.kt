795a6b6f24003fd2f24190bb692e9eeb930c98c4
PresentKim <debe3721@gmail.com>
2021-07-24 00:38:30 +0900

    Implements PlayerChunkLoader

---
+++ app/src/main/kotlin/be/zvz/kookie/player/PlayerChunkLoader.kt
@@ -0,0 +1,26 @@
+/**
+ *
+ * _  __           _    _
+ * | |/ /___   ___ | | _(_) ___
+ * | ' // _ \ / _ \| |/ / |/ _ \
+ * | . \ (_) | (_) |   <| |  __/
+ * |_|\_\___/ \___/|_|\_\_|\___|
+ *
+ * A server software for Minecraft: Bedrock Edition
+ *
+ * Copyright (C) 2021 organization Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package be.zvz.kookie.player
+
+import be.zvz.kookie.math.Vector3
+import be.zvz.kookie.world.TickingChunkLoader
+
+class PlayerChunkLoader(var currentLocation: Vector3) : TickingChunkLoader {
+    override val x: Int get() = currentLocation.x.toInt()
+    override val z: Int get() = currentLocation.z.toInt()
+}
