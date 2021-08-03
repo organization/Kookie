21194f52e24374da432ecef53a982a7e5d169883
PresentKim <debe3721@gmail.com>
2021-08-03 20:15:39 +0900

    FallableHelper : Pre-implements onNearbyBlockChange()

--- app/src/main/kotlin/be/zvz/kookie/block/utils/FallableHelper.kt
+++ app/src/main/kotlin/be/zvz/kookie/block/utils/FallableHelper.kt
@@ -1,5 +1,8 @@
 package be.zvz.kookie.block.utils
 
+import be.zvz.kookie.block.Block
+import be.zvz.kookie.block.VanillaBlocks
+import be.zvz.kookie.math.Facing
 import be.zvz.kookie.world.Position
 
 interface FallableHelper {
@@ -7,8 +10,20 @@ interface FallableHelper {
     fun getId(): Int
     fun getMeta(): Int
 
-    fun onNearbyBlockChange(): Unit {
-        var pos = getPos()
-        //TODO: not yet implemented
+    fun onNearbyBlockChange() {
+        pos.world?.let { world ->
+            val down = world.getBlock(pos.getSide(Facing.DOWN))
+            if (down.getId() == VanillaBlocks.AIR.id /* TODO: || down is Liquid || down is Fire */) {
+                world.setBlock(pos, VanillaBlocks.AIR.block)
+
+                if (this !is Block)
+                    throw IllegalStateException("FallableHelper should only be used by Blocks")
+
+                /** TODO: Implement after implementing FallingBlock
+                 * val fall = FallingBlock(Location.fromObject(pos.add(0.5, 0.0, 0.5), world, 0), this)
+                 * fall.spawnToAll()
+                 */
+            }
+        }
     }
 }
